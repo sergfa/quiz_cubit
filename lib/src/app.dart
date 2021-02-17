fa17dbd47d515e809ccfaed3d0034de91153cf9d
sfayman <Sergey_Fayman@bmc.com>
2021-02-17 16:40:37 +0200

    Update theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,7 +8,7 @@ import 'package:quiz_cubit/src/presentation/screens/home_screen.dart';
 class App extends StatelessWidget {
   final AppRouter appRouter;
   App({@required this.appRouter});
-  
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider<ScoreCubit>(
@@ -16,7 +16,10 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'Quiz Cubit',
         theme: ThemeData.light().copyWith(
-            textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme)),
+          textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
+          primaryColor: Color(0xff689F38),
+          accentColor: Color(0xff4CAF50),
+        ),
         home: HomeScreen(),
         onGenerateRoute: appRouter.onGenerateRoute,
       ),
